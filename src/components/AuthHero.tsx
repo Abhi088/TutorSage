73f4aa9f41ade9ec0973ab4d33405341fdd8e51b
Abhi088 <abhicoding8@gmail.com>
2021-07-20 19:11:37 +0530

    Added Auth hero image

--- src/components/AuthHero.tsx
+++ src/components/AuthHero.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { FC, memo } from 'react';
+import AuthHeroImg from '../images/AuthHeroImg.webp';
 
 interface Props {
     className?: string;
@@ -11,7 +12,9 @@ const AuthHero: FC<Props> = ({ className }) => {
         console.log("AuthHero Rendering for the first time");
     }, []);
     return (
-        <div className={`h-screen w-full bg-black text-white ${className}`}>Logo will go here</div>
+        <div className={`h-screen w-full bg-black text-white ${className}`} style={{ backgroundColor: "#060818" }}>
+            <img className="m-auto h-full w-full" src={AuthHeroImg} alt="Logo is here"></img>
+        </div>
     );
 };
 
