2a04b7e2b1ba211d36e08cec7eb8bb6c1582ed10
Abhi088 <abhicoding8@gmail.com>
2021-08-04 11:43:21 +0530

    Added Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { FC, memo } from 'react';
+import Avatar from '../components/Avatar/Avatar';
+import LinkTo from '../components/LinkTo';
+
+interface Props {
+    profileImg: string;
+}
+
+const Navbar: FC<Props> = ({ profileImg }) => {
+
+    const [showProfileMenu, setShowProfileMenu] = useState(false);
+
+    return (
+        <div className="flex flex-row justify-between px-10 py-2 bg-gray-900 items-center">
+            <h1 className="text-white text-xl font-semibold">CODEBITS</h1>
+            <div onClick={() => { setShowProfileMenu(!showProfileMenu) }}>
+                <Avatar imgSrc={profileImg} shape="square" showStatus={false} ></Avatar>
+                <div className={`${(showProfileMenu ? "visible" : "hidden")}`}>
+                    <ul>
+                        <li><LinkTo to="/profile" text="Profile" /></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Navbar.defaultProps = {};
+
+export default memo(Navbar);
--- src/pages/AppContainer/AppContainer.page.tsx
+++ src/pages/AppContainer/AppContainer.page.tsx
@@ -4,25 +4,29 @@ import Sidebar from '../../components/Sidebar';
 import DashboardPage from './Dashboard.page';
 import RecordingsPage from './Recordings.page';
 import LecturePage from './Lecture.page';
+import Navbar from '../../components/Navbar';
 
 interface Props {
 }
 
 const AppContainer: FC<Props> = (props) => {
     return (
-        <div className="flex flex-row">
-            <Sidebar></Sidebar>
-            <Switch>
-                <Route path='/dashboard'>
-                    <DashboardPage></DashboardPage>
-                </Route>
-                <Route path='/recordings'>
-                    <RecordingsPage></RecordingsPage>
-                </Route>
-                <Route path="/batch/:batchNumber/lecture/:lectureNumber">
-                    <LecturePage></LecturePage>
-                </Route>
-            </Switch>
+        <div>
+            <Navbar profileImg="https://designreset.com/cork/ltr/demo4/assets/img/profile-1.jpeg"></Navbar>
+            <div className="flex flex-row">
+                <Sidebar></Sidebar>
+                <Switch>
+                    <Route path='/dashboard'>
+                        <DashboardPage></DashboardPage>
+                    </Route>
+                    <Route path='/recordings'>
+                        <RecordingsPage></RecordingsPage>
+                    </Route>
+                    <Route path="/batch/:batchNumber/lecture/:lectureNumber">
+                        <LecturePage></LecturePage>
+                    </Route>
+                </Switch>
+            </div>
         </div>
     );
 };
